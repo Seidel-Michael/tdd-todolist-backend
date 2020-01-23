c63d0536944d04210d6b2aee82a4117f821be4ca
Seidel-Michael <michael.seidel@balluff.de>
2020-01-23 15:54:36 +0100

    Implement simple hostin in index file

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@ import { DbConnection } from './db/db-connection';
 import { createPool } from 'slonik';
 import { Api } from './api/api';
 
-/*const pool = createPool('postgresql://postgres:mysecretpassword@localhost:5432/todolist');
+const pool = createPool('postgresql://postgres:mysecretpassword@localhost:5432/todolist');
 const connection = new DbConnection(pool);
 
 const api = new Api(connection);
+const port = process.env.port || 8080;
 
-const PORT = 8080;
-
-api.app.listen(PORT)
+api.app.listen(port)
     .on('error', err => {
+        // tslint:disable-next-line:no-console
         console.error(err);
     })
     .on('listening', () => {
-        console.log(`Server listening on port ${PORT}`);
-    });*/
+        // tslint:disable-next-line:no-console
+        console.log(`Server listening on port ${port}`);
+    });
